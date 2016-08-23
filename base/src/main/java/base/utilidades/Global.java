fb43613a9848092e2f9609217ae0976337a287e5
Omar Paché <omar.pache@gmail.com>
2016-08-22 21:08:39 -0300

    Globales

--- base/src/main/java/base/utilidades/Global.java
+++ base/src/main/java/base/utilidades/Global.java
@@ -1,7 +1,5 @@
 package base.utilidades;
 
-import javax.persistence.EntityManagerFactory;
-
 /**
  * Clase contenedora de las constantes
  *
@@ -11,10 +9,4 @@ public class Global {
 
     // Constante inyectada desde el archivo de propiedades
     public static String ambiente;
-    public static String dataBase = null;
-    public static String dataBaseUrl = null;
-    public static int dataBasePort;
-    public static String dataBaseUser = "root";
-    public static String dataBasePassword = "toor";
-    public static EntityManagerFactory emf = null;
 }
